2ca0db0da7f1cb3157c98d148268bf92451097a8
adrien <kikuiredkill@gmail.com>
2017-02-14 06:26:34 +0100

    fixe position portrait
    tout les layouts locker en portrait

--- LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MainMenu.java
+++ LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MainMenu.java
@@ -9,13 +9,16 @@ import android.widget.Button;
 public class MainMenu extends AppCompatActivity {
 
     private Button button_game;
+    private Button button_success;
+    private Button button_skils;
+    private Button button_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        button_game = (Button) findViewById(R.id.button_jeux);
+        button_game = (Button) findViewById(R.id.button_game);
         button_game.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -24,5 +27,36 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        button_success = (Button) findViewById(R.id.button_success);
+        button_success.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, MenuSuccess.class);
+                startActivity(intent);
+            }
+        });
+
+        button_skils = (Button) findViewById(R.id.button_skils);
+        button_skils.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, MenuSkils.class);
+                startActivity(intent);
+            }
+        });
+
+        button_settings = (Button) findViewById(R.id.button_settings);
+        button_settings.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, MenuSettings.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
