158f21fdbebb76f8455dbb34710423ae52230316
AymericHuetDeFroberville <adefrob@gmail.com>
2017-02-14 03:28:17 +0100

    layout menu memory et memory link
    connnection du layout menu memory et du jeux memory

--- LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MenuMemory.java
+++ LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MenuMemory.java
@@ -1,13 +1,33 @@
 package com.example.kikui.lepetitfranais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuMemory extends AppCompatActivity {
+
+    private Button buttonMemory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_memory);
+
+        buttonMemory = findViewById(R.id.button_memory_animaux);
+
+        buttonMemory.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(buttonMemory.this, Memory.class)
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
