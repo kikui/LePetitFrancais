fb2cd2878ba7733c9de67a34d96c6fae5628721b
adrien <kikuiredkill@gmail.com>
2017-02-14 03:19:53 +0100

    commit sympatosh
    du sale mamen !

--- LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MainMenu.java
+++ LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MainMenu.java
@@ -1,13 +1,28 @@
 package com.example.kikui.lepetitfranais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button button_game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        button_game = (Button) findViewById(R.id.button_jeux);
+        button_game.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, MenuGame.class);
+                startActivity(intent);
+            }
+        });
     }
 }
