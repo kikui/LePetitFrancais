7626276e451755e93e2acc4ab4c13a81d208f7c8
adrien <kikuiredkill@gmail.com>
2017-02-14 04:29:05 +0100

    commit design
    redesign menu game

--- LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MenuGame.java
+++ LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/MenuGame.java
@@ -1,13 +1,29 @@
 package com.example.kikui.lepetitfranais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableRow;
 
 public class MenuGame extends AppCompatActivity {
+
+    private TableRow RowPlayMemory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_game);
+
+        RowPlayMemory = (TableRow) findViewById(R.id.playMenuMemory);
+        RowPlayMemory.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuGame.this, MenuMemory.class);
+                startActivity(intent);
+            }
+        });
     }
 }
