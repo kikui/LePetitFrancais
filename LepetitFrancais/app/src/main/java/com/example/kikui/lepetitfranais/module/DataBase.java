c9e458fd752762e8ac1fac4d42bec4eb75322e90
adrien <kikuiredkill@gmail.com>
2017-02-23 16:54:05 +0100

    mise en place d'une base de donnée
    DataBase

---
+++ LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/module/DataBase.java
@@ -0,0 +1,41 @@
+package com.example.kikui.lepetitfranais.module;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by kikui on 23/02/2017.
+ */
+
+public class DataBase extends SQLiteOpenHelper {
+
+    public static final String SCORE_KEY = "id";
+    public static final String SCORE_JEUX = "jeux";
+    public static final String SCORE_SCORES = "scores";
+
+    public static final String SCORE_TABLE_NAME = "Score_par_jeu";
+    public static final String SCORE_TABLE_CREATE =
+            "CREATE TABLE " + SCORE_TABLE_NAME + " (" +
+                    SCORE_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    SCORE_JEUX + " TEXT, " +
+                    SCORE_SCORES + " INTEGER DEFAULT 0);";
+    public static final String SCORE_TABLE_DROP = "DROP TABLE IF EXISTS " + SCORE_TABLE_NAME + ";";
+
+    public DataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SCORE_TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SCORE_TABLE_DROP);
+        onCreate(db);
+    }
+
+
+}
--- LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/module/ScoresAnimaux.java
+++
@@ -1,8 +0,0 @@
-package com.example.kikui.lepetitfranais.module;
-
-/**
- * Created by kikui on 22/02/2017.
- */
-
-public class ScoresAnimaux {
-}
