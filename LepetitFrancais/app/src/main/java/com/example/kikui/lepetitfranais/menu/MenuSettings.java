62217459d2befa35ad45ed1e53d3ce8fa62d856c
adrien <kikuiredkill@gmail.com>
2017-02-25 23:41:02 +0100

    commit pas tres utile

--- LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/menu/MenuSettings.java
+++ LepetitFrancais/app/src/main/java/com/example/kikui/lepetitfranais/menu/MenuSettings.java
@@ -29,51 +29,36 @@ public class MenuSettings extends AppCompatActivity {
 
         //-----------------------------------------------------------------
 
-        //Création d'une instance de ma classe JeuBDD
         JeuBDD jeuBdd = new JeuBDD(this);
 
-        //Création d'un jeu
         Jeu memoryAnimaux = new Jeu("memoryAnimaux",1500);
 
-        //On ouvre la base de données pour écrire dedans
         jeuBdd.open();
-        //On insère le livre que l'on vient de créer
+
         jeuBdd.insertJeu(memoryAnimaux);
 
-        //Pour vérifier que l'on a bien créé notre jeu dans la BDD
-        //on extrait le jeu de la BDD grâce au nom du jeu que l'on a créé précédemment
         Jeu jeuFromBdd = jeuBdd.getJeuWithNameJeu(memoryAnimaux.getJeu());
-        //Si un livre est retourné (donc si le jeu à bien été ajouté à la BDD)
+
         if(jeuFromBdd != null){
-            //On affiche les infos du jeu dans un Toast
             Toast.makeText(this, jeuFromBdd.toString(), Toast.LENGTH_LONG).show();
-            //On modifie le titre du livre
             jeuFromBdd.setJeu("J'ai modifié le nom du jeu");
-            //Puis on met à jour la BDD
             jeuBdd.updateJeu(jeuFromBdd.getId(), jeuFromBdd);
         }
 
-        //On extrait le livre de la BDD grâce au nouveau titre
         jeuFromBdd = jeuBdd.getJeuWithNameJeu("J'ai modifié le nom du jeu");
-        //S'il existe un livre possédant ce titre dans la BDD
+
         if(jeuFromBdd != null){
-            //On affiche les nouvelle info du livre pour vérifié que le titre du livre a bien été mis à jour
             Toast.makeText(this, jeuFromBdd.toString(), Toast.LENGTH_LONG).show();
-            //on supprime le livre de la BDD grâce à son ID
             jeuBdd.removeJeuWithID(jeuFromBdd.getId());
         }
 
-        //On essait d'extraire de nouveau le livre de la BDD toujours grâce à son nouveau titre
         jeuFromBdd = jeuBdd.getJeuWithNameJeu("J'ai modifié le nom du jeu");
-        //Si aucun livre n'est retourné
+
         if(jeuFromBdd == null){
-            //On affiche un message indiquant que le livre n'existe pas dans la BDD
             Toast.makeText(this, "Ce livre n'existe pas dans la BDD", Toast.LENGTH_LONG).show();
         }
 
-        //Si le livre existe (mais normalement il ne devrait pas)
         else{
-            //on affiche un message indiquant que le livre existe dans la BDD
             Toast.makeText(this, "Ce livre existe dans la BDD", Toast.LENGTH_LONG).show();
         }
 
